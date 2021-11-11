313de140fb8a6ea9ce913892154d8d1d5931cf3d
leandro.paixao <leandro.paixao@objective.com.br>
2021-11-11 15:58:55 -0300

    fix: include document basic

---
+++ src/pages/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import BasicLayout from "layout/globalStyles";
+
+function HomePage() {
+  return (
+    <BasicLayout>
+      <div>Welcome to Next.js !!</div>
+    </BasicLayout>
+  );
+}
+
+export default HomePage;
